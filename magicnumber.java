1f849d0052702e0c5b77cf8c52a395f309b29279
Ganesh041111 <Ganeshshingre07@gmail.com>
2023-06-30 23:57:44 +0530

    Magic number

---
+++ magicnumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class magicnumber{
+    public static int magic(int num, int ans){
+        while(num>0){
+            ans=ans+(num%10);
+            num=num/10;
+        }
+
+        if(ans>9){
+            num=ans;
+            ans=0;
+            System.out.println(num+" "+ans);
+            magic(num,ans);
+        }
+        return ans;
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int ans=0;
+        int val=magic(num,ans);
+        if(val==1){
+            System.out.println("Given number "+num+" is a magic number.");
+        }
+        else{
+            System.out.println("Given number "+num+" is not a magic number.");
+        }
+    }
+}
